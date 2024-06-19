0d52a3b293db12943565927481f85199afeddb9b
laiscrz <lais.albino2003@gmail.com>
2024-06-19 17:31:30 -0300

    feat: add response message server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import { createServer } from 'node:http'
 
-const server = createServer(()=>{
-    console.log("Servidor ativo !")
+const server = createServer((request, response)=>{
+    response.write("Servidor ativo !");
+
+    return response.end();
 });
 
 server.listen(3000);
