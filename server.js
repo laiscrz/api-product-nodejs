9ca44ebc4c4cb4e45dd1ae883cb8a7456dd86732
laiscrz <lais.albino2003@gmail.com>
2024-06-20 10:13:13 -0300

    feat(api): implement product creation endpoint using DatabaseMemory

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import fastify from 'fastify';
+import { DatabaseMemory } from './database-memory.js';
 
 const server = fastify();
+
+const database = new DatabaseMemory();
 
 //GET
 server.get('/products', () => {
@@ -8,8 +11,19 @@ server.get('/products', () => {
 });
 
 // POST
-server.post('/products', () => {
-    return 'Servidor ativo !'
+server.post('/products', (request, reply) => {
+    database.create({
+        name: "Produto 01",
+        description: "Descrição do Produto 01",
+        manufacturer: "XYZ Inc.",
+        category: "Eletrônicos",
+        price: 99.10,
+        available: true
+    });
+
+    console.log(database.list());
+
+    return reply.status(201).send(); // CREATED
 });
 
 // PUT
