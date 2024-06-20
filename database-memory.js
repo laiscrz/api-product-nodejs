b97e0e8070811244e822c904fa406168890587a4
laiscrz <lais.albino2003@gmail.com>
2024-06-20 09:51:20 -0300

    feat: create methods in database-memory.js

--- database-memory.js
+++ database-memory.js
@@ -1,3 +1,11 @@
 export class DatabaseMemory {
+    #products = []
 
+    create(product) {
+        this.#products.push(product);
+    }
+
+    update(id, product) {
+        this.#products.push(product);
+    }
 };
