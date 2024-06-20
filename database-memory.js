90f02031669fe238e513ce6a0e4da55b87183655
laiscrz <lais.albino2003@gmail.com>
2024-06-20 09:57:32 -0300

    refactor(database): update DatabaseMemory with Map storage and UUID generation

--- database-memory.js
+++ database-memory.js
@@ -1,11 +1,22 @@
+import { randomUUID } from 'node:crypto'
 export class DatabaseMemory {
-    #products = []
+    #products = new Map();
+
+    list( ) {
+       return this.#products.values();
+    }
 
     create(product) {
-        this.#products.push(product);
+        const productId = randomUUID();
+
+        this.#products.set(productId, product);
     }
 
     update(id, product) {
-        this.#products.push(product);
+        this.#products.set(id, product);
+    }
+
+    delete(id) {
+        this.#products.delete(id);
     }
 };
